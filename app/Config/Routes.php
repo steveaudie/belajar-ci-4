c894e05766c0faa6a25485fa13a872e17ecc5ba6
steveaudie <steveaudie553@gmail.com>
2025-06-22 22:34:19 +0700

    tugas migrate and seed

---
+++ app/Config/Routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
---
+++ app/Views/components/sidebar.php
@@ -0,0 +1,35 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == '') ? "" : "collapsed" ?>" href="/">
+                <i class="bi bi-grid"></i>
+                <span>Home</span>
+            </a>
+        </li><!-- End Home Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == 'keranjang') ? "" : "collapsed" ?>" href="keranjang">
+                <i class="bi bi-cart-check"></i>
+                <span>Keranjang</span>
+            </a>
+        </li><!-- End Keranjang Nav -->
+
+        <?php if (session()->get('role') == 'admin') : ?>
+            <li class="nav-item">
+                <a class="nav-link <?php echo (uri_string() == 'produk') ? "" : "collapsed" ?>" href="produk">
+                    <i class="bi bi-receipt"></i>
+                    <span>Produk</span>
+                </a>
+            </li><!-- End Produk Nav -->
+        <?php endif; ?>
+        <li class="nav-item">
+    <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+        <i class="bi bi-person"></i>
+        <span>Profile</span>
+    </a>
+</li>
+        
+    </ul>
+</aside><!-- End Sidebar -->
