a967f55d1d2001f573e0128023ef5589195ccc35
steveaudie <steveaudie553@gmail.com>
2025-06-22 22:39:31 +0700

    tugas migrate and seed

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -26,5 +26,16 @@ $routes->get('ProductCategory', 'ProductCategoryController::index');
 $routes->post('ProductCategory', 'ProductCategoryController::create');
 $routes->post('ProductCategory/edit/(:any)', 'ProductCategoryController::edit/$1');
 $routes->get('ProductCategory/delete/(:any)', 'ProductCategoryController::delete/$1');
-
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
+$routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+$routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'TransaksiController::index');
+    $routes->post('', 'TransaksiController::cart_add');
+    $routes->post('edit', 'TransaksiController::cart_edit');
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+    $routes->get('clear', 'TransaksiController::cart_clear');
+   
+});
 
